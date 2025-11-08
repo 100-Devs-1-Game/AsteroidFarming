864c0551b9712a3a6efc6bc8d956cda0fa26ec97
Mouse Potato Does Stuff <mousepotato@tutanota.com>
2025-11-08 19:59:38 +0100

    Camera

--- src/scenes/main_camera.gd
+++ src/scenes/main_camera.gd
@@ -1,5 +1,8 @@
 class_name MainCamera extends Camera3D
 
+@export var move_speed:=5.0
+@export var rotate_speed:=1.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -7,4 +10,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var base_direction:=Input.get_vector("move_left","move_right","move_forward","move_back")
+	base_direction=base_direction.normalized()
+	var forward:=global_transform.basis.z*Vector3(1,0,1)
+	var right:=global_transform.basis.x*Vector3(1,0,1)
+	var movement:=Vector3.ZERO
+	movement+=right*base_direction.x
+	movement+=forward*base_direction.y
+	movement*=delta*move_speed
+	position+=movement
+	
+	var turn_direction:=Input.get_axis("spin_left","spin_right")
+	rotate_y(turn_direction*delta*rotate_speed)
