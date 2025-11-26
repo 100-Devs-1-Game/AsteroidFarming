134f25407c4fc0662dc1a6f059cad285efde7aeb
Mouse Potato Does Stuff <mousepotato@tutanota.com>
2025-11-26 20:14:47 +0100

    made leaderboard input screen

--- src/scripts/score_menu.gd
+++ src/scripts/score_menu.gd
@@ -1,7 +1,12 @@
 class_name ScoreMenu extends SwappableMenu
 
 @export var display: DisplayScore
+@export var name_input: TextEdit
 
 func enter(new_data:Dictionary)->void:
 	self.display.display_score(new_data.get("result",{}))
 	self.show()
+
+
+func submit_to_leaderboard() -> void:
+	var name:String=name_input.text
