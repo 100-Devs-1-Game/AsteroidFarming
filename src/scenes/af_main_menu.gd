6cf6879093d41992e0e39c449a30e94c9eeb2bed
Mouse Potato Does Stuff <mousepotato@tutanota.com>
2025-11-29 10:27:58 +0100

    changed fonts and added a credits menu

--- src/scenes/af_main_menu.gd
+++ src/scenes/af_main_menu.gd
@@ -4,6 +4,7 @@ class_name AsteroidFarmingMainMenu extends SwappableMenu
 @export var title: String
 @export var options_button:Button
 @export var options_menu:SwappableMenu
+@export var credits_menu:SwappableMenu
 @export var leaderboard_menu:SwappableMenu
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,3 +27,7 @@ func _on_play_pressed() -> void:
 
 func _on_leaderboard_pressed() -> void:
 	self.sig_swap.emit(leaderboard_menu,true)
+
+
+func _on_credits_pressed() -> void:
+	self.sig_swap.emit(credits_menu,true)
