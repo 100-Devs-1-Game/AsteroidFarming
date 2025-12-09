e1541761cd016430ddfba116986dec2641a2d2a7
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-09 17:37:13 +0100

    UI overhaul

--- src/scenes/game_menu.gd
+++ src/scenes/game_menu.gd
@@ -7,19 +7,7 @@ signal sig_exit_to_menu
 signal sig_choose_tool(ind: int)
 @export var score_node: DisplayScore
 
-@export var tools: OptionButton
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	tools.clear()
-	for name in FarmConstants.TOOL_NAMES:
-		tools.add_item(name)
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func interact_with_game():
 	sig_interact.emit()
