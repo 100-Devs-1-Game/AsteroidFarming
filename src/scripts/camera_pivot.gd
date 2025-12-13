a9facae69ec02d48826b118c8c411798545a4149
galesy <galesyfailsy@gmail.com>
2025-12-13 15:46:55 -0300

    camera spins faster on Shiftkey held

--- src/scripts/camera_pivot.gd
+++ src/scripts/camera_pivot.gd
@@ -2,7 +2,10 @@ extends Node3D
 
 func _process(delta: float) -> void:
 	if !owner.is_processing(): return
+	var value = delta
+	if Input.is_action_pressed("shift"):
+		value *= 3
 	if Input.is_action_pressed("move_left"):
-		rotate_y(-delta)
+		rotate_y(-value)
 	elif Input.is_action_pressed("move_right"):
-		rotate_y(delta)
+		rotate_y(value)
